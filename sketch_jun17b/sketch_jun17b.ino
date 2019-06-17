e60168e7f098875969961fe5f7c9e600c89b6676
Shubham-Kumar-2000 <41825906+Shubham-Kumar-2000@users.noreply.github.com>
2019-06-18 04:54:56 +0530

    Add files via upload

---
+++ sketch_jun17b/sketch_jun17b.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(4,OUTPUT);
+pinMode(12,OUTPUT);
+pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(4,HIGH);
+digitalWrite(12,HIGH);
+digitalWrite(9,HIGH);
+}
